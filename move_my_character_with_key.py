da0efbe4172cbf888bc8cfbc18709402408b7185
2021182046 <lope32@naver.com>
2023-09-27 22:37:36 +0900

    Update Y Animation
    Y축 이동 애니메이션 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,7 +5,7 @@ run_character = load_image('robot_run.png')
 tuk_ground = load_image('TUK_GROUND.png')
 
 def handle_events():
-    global running, dir
+    global running, dir, dir_y
 
     events = get_events()
     for event in events:
@@ -16,6 +16,10 @@ def handle_events():
                 dir += 1
             elif event.key == SDLK_LEFT:
                 dir -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -23,12 +27,17 @@ def handle_events():
                 dir -= 1
             elif event.key == SDLK_LEFT:
                 dir += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
 
 running = True
 x = 800 // 2
 y = 600 // 2
 frame = 0
 dir = 0
+dir_y = 0
 
 while running:
     clear_canvas()
@@ -38,6 +47,7 @@ while running:
     handle_events()
     frame = (frame+1) % 8
     x += dir * 5
+    y += dir_y * 5
     delay(0.05)
 
 
