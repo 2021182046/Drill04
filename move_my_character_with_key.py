f4ec24b366b359510997e8fd7ed000a3cf255acf
2021182046 <lope32@naver.com>
2023-09-27 21:53:19 +0900

    Update File And Animation
    로봇파일 사용 및 좌우 running 구현

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,28 +1,36 @@
 from pico2d import *
 
-
 open_canvas()
-grass = load_image('grass.png')
-character = load_image('animation_sheet.png')
-
+run_character = load_image('robot_run.png')
 
 def handle_events():
     global running
-
-    # fill here
+    global x
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # fill here
-
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                x += 10
+            elif event.key == SDLK_LEFT:
+                x -= 10
+            elif event.key == SDLK_ESCAPE:
+                running = False
 
 running = True
 x = 800 // 2
+y = 600 // 2
 frame = 0
 
-# fill here
+while running:
+    clear_canvas()
+    run_character.clip_draw(frame*63, 0, 60, 70, x, y)
+    update_canvas()
+    handle_events()
+    frame = (frame+1) % 8
+    delay(0.05)
 
 
 close_canvas()
